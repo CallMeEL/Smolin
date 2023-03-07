d9278f54e37b27e3c0af053fe929dc19a036bb71
CallMeEL <ivipgra2@gmail.com>
2023-03-07 13:29:37 +0700

    update css

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,32 +7,7 @@
     <title>SMOLIN</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <style>
-        .banner {
-        background: url("{{ ('img/hoome.png') }}")
-        no-repeat center center;
-        background-size: cover;
-        padding-top: 20%;
-        padding-bottom: 20%;
-        color: #fff;
-    }
-    div.top{
-        position: absolute;
-        top: 150px;
-    }
-    .colorpink{
-        color: #fc2585;
-    }
-    .blur {
-        -webkit-backdrop-filter: blur(8px);
-        backdrop-filter: blur(8px);
-    }
-    .shadow-5-strong {
-        -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-        -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-    }
-    </style>
+    <link rel="stylesheet" href="{{ ('assets/beranda.css') }}">
 </head>
 <body>
     <nav class="navbar fixed-top blur navbar-expand-lg shadow-5-strong">
