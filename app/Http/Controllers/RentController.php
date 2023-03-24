0046abef92420b28b16d8d5cc2a551ac4c32c83a
CallMeEL15 <63996382+CallMeEL@users.noreply.github.com>
2023-03-24 13:20:09 +0700

    add Rent and Invoice model

---
+++ app/Http/Controllers/RentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rent;
+use App\Http\Requests\StoreRentRequest;
+use App\Http\Requests\UpdateRentRequest;
+
+class RentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rent $rent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rent $rent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRentRequest $request, Rent $rent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rent $rent)
+    {
+        //
+    }
+}
