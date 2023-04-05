2d9229fe3c48166bfe38aa61741fa5a9d2bdc336
FarikNabil <fariknabil0@gmail.com>
2023-04-05 11:51:44 +0700

    ya

--- app/Http/Controllers/MotorController.php
+++ app/Http/Controllers/MotorController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Motor;
-use App\Models\Rent;
+use App\Models\RentLog;
 
 class MotorController extends Controller
 {
@@ -45,6 +45,40 @@ class MotorController extends Controller
 
     public function rent(Request $request, Motor $motor)
     {
+        $validateData = $request->validate([
+            'rent_date' => 'required|date',
+            'return_date' => 'required|date',
+        ]);
+
+        // Jika pengisian tanggal sewa lebih besar dari tanggal kembali
+        if ($validateData['rent_date'] > $validateData['return_date']) {
+            $request->session()->flash('error', 'Tanggal Sewa tidak boleh lebih dari Tanggal Kembali!');
+            return redirect('/motor/' . $motor->id);
+        }
+
+        // Jika tanggal sewa adalah kemarin maka tidak bisa
+        if ($validateData['rent_date'] < date('Y-m-d')) {
+            $request->session()->flash('error', 'Tanggal Sewa tidak boleh sebelum hari ini!');
+            return redirect('/motor/' . $motor->id);
+        }
+
+        // Jika tanggal booking sewa pada tanggal yang sama, maka tidak bisa menyewa
+        $rentLog = RentLog::where('motor_id', $motor->id)
+            ->where('rent_date', $validateData['rent_date'])
+            ->where('return_date', $validateData['return_date'])
+            ->first();
+        if ($rentLog) {
+            $request->session()->flash('error', 'Tanggal Sewa dan Tanggal Kembali sudah ada!');
+            return redirect('/motor/' . $motor->id);
+        }
+
+        $validateData['motor_id'] = $motor->id;
+        $validateData['user_id'] = auth()->user()->id;
+
+        RentLog::create($validateData);
+        $request->session()->flash('success', 'Silahkan melakukan pembayaran');
+        // Kembali ke home
+        return redirect('/');
 
     }
 
