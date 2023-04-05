367d6ac4be857f3b4ce31cc2076c27c067c1de1a
FarikNabil <fariknabil0@gmail.com>
2023-04-05 10:16:31 +0700

    push

--- resources/views/beranda/layout.blade.php
+++ resources/views/beranda/layout.blade.php
@@ -1,91 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ ('assets/beranda.css') }}">
+    <link rel="stylesheet" href="{{ 'assets/beranda.css' }}">
 </head>
+
 <body>
-<!-- Navbar -->
+    <!-- Navbar -->
     <nav class="navbar fixed-top blur navbar-expand-lg shadow-5-strong">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <strong class="text-white" >Smolin</strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav navbar-right ms-auto mb-2 mb-lg-0">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <strong class="text-white">Smolin</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav navbar-right ms-auto mb-2 mb-lg-0">
 
-            @auth
+                    @auth
 
-            @can('admin')
+                        @can('admin')
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('admin') }}">Admin</a>
+                            </li>
+                        @endcan
 
-            <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('admin') }}">Admin</a>
-            </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuHome')" aria-current="page"
+                                href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <button type="submit" class="btn alert-transparent-background text-white">Logout</button>
+                            </form>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuHome')" aria-current="page"
+                                href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn transparent-background text-white" aria-current="page"
+                                href="/login"><strong>Login</strong></a>
+                        </li>
 
-            @endcan
+                    @endauth
 
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuHome')" aria-current="page" href="{{ route('home') }}">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="btn alert-transparent-background text-white">Logout</button>
-                </form>
-            </li>
-
-            @else
-
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuHome')" aria-current="page" href="{{ route('home') }}">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-            <a class="btn transparent-background text-white" aria-current="page" href="/login"><strong>Login</strong></a>
-            </li>
-
-            @endauth
-
-        </ul>
+                </ul>
+            </div>
         </div>
-    </div>
     </nav>
-<!-- Content -->
+    <!-- Content -->
 
     @auth
 
         @yield('content')
-
-
     @else
+        <div class="container-fluid banner">
+            <div class="container-fluid top">
 
-    <div class="container-fluid banner">
-        <div class="container-fluid top">
+                @yield('content')
 
-        @yield('content')
-
+            </div>
         </div>
-    </div>
 
     @endauth
 
@@ -95,4 +98,5 @@
     </footer>
 
 </body>
+
 </html>
--- resources/views/beranda/sewa-motor.blade.php
+++ resources/views/beranda/sewa-motor.blade.php
@@ -1,99 +1,112 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Detail Motor {{ $result->nama_motor }}</title>
     <link rel="stylesheet" href="{{ asset('assets/beranda.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
+
 <body class="banner-auth">
 
-<!-- Navbar -->
-<nav class="navbar fixed-top blur navbar-expand-lg shadow-5-strong">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <strong class="text-white" >Smolin</strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav navbar-right ms-auto mb-2 mb-lg-0">
+    <!-- Navbar -->
+    <nav class="navbar fixed-top blur navbar-expand-lg shadow-5-strong">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <strong class="text-white">Smolin</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav navbar-right ms-auto mb-2 mb-lg-0">
 
-            @auth
+                    @auth
 
-            @can('admin')
+                        @can('admin')
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('admin') }}">Admin</a>
+                            </li>
+                        @endcan
 
-            <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('admin') }}">Admin</a>
-            </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuHome')" aria-current="page"
+                                href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <button type="submit" class="btn alert-transparent-background text-white">Logout</button>
+                            </form>
+                        </li>
 
-            @endcan
+                    @endauth
 
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuHome')" aria-current="page" href="{{ route('home') }}">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuProfile')" aria-current="page" href="/profile">Profile</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link text-white @yield('menuContact')" aria-current="page" href="#">Contact</a>
-            </li>
-            <li class="nav-item">
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="btn alert-transparent-background text-white">Logout</button>
-                </form>
-            </li>
-
-            @endauth
-
-        </ul>
+                </ul>
+            </div>
         </div>
-    </div>
     </nav>
 
-<div class="container">
+    <!-- Isi -->
+    <div class="container">
 
-    <div class="row align-items-center">
+        <div class="row">
 
-        <div class="card transparent-form-profile border-grey border-rounded">
-            <div class="card-header">
-                {{-- Gambar Motor --}}
-                <h1 class="card-title">
-                    <img src="{{ asset('storage/'.$result->gambar_motor) }}" alt="gambar motor" class="border-rounded" style="width: 125px">
-                    <strong>{{ $result->nama_motor }}</strong>
-                </h1>
+            <div class="card col-md-6 transparent-form-profile border-grey border-rounded">
+                <div class="card-header">
+                    {{-- Gambar Motor --}}
+                    <h1 class="card-title">
+                        <img src="{{ asset('storage/' . $result->gambar_motor) }}" alt="gambar motor"
+                            class="border-rounded" style="width: 125px">
+                        <strong>{{ $result->nama_motor }}</strong>
+                    </h1>
+                </div>
+                <div class="card-body">
+                    {{-- Transmisi --}}
+                    <h3 class="card-title">
+                        Transmisi : <strong>{{ $result->tipe_motor }}</strong>
+                    </h3>
+                    {{-- Harga --}}
+                    <h3 class="card-title">
+                        Harga : <strong>{{ $result->harga_motor }} /hari</strong>
+                    </h3>
+                </div>
+                <div class="card-footer">
+                    {{-- Tombol Sewa --}}
+                    <form action="" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Sewa</button>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-                {{-- Transmisi --}}
-                <h3 class="card-title">
-                    Transmisi : <strong>{{ $result->tipe_motor }}</strong>
-                </h3>
-                {{-- Harga --}}
-                <h3 class="card-title">
-                    Harga : <strong>{{ $result->harga_motor }} /hari</strong>
-                </h3>
+
+            <div class="col-md-6">
+                <img style="width: 100%" src="{{ 'img/motor_header.png' }}">
             </div>
-            <div class="card-footer">
-                {{-- Tombol Sewa --}}
-                <form action="" method="POST">
-                    @csrf
-                    <button type="submit" class="btn btn-danger">Sewa</button>
-                </form>
-            </div>
+
         </div>
 
     </div>
-
-</div>
 
 </body>
 <footer class="container-fluid text-center pt-5 pb-5">
     All Rights Reserved &copy; 2023
 </footer>
+
 </html>
